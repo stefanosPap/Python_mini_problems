3b92ca631bf9cb2e8a861e166f9430f5248f5264
Stefanos <stefanospapadam@gmail.com>
2020-02-20 11:23:10 +0200

    Final pig's version - without optimization - slow implementation

--- Pigs-In-A-Pen/pigs.py
+++ Pigs-In-A-Pen/pigs.py
@@ -2,8 +2,46 @@ class Game():
     
     def __init__(self, board):
         self.board = board
-        
+    
+    def loop(self,lines,packages):
+        lines_temp = lines 
+        for i in range(len(packages)):
+            count = 0
+            lines_temp = sorted(lines_temp) 
+            for j in range(len(lines_temp)):
+                if lines_temp[j] == packages[i][0] or lines_temp[j] == packages[i][1] or lines_temp[j] == packages[i][2] or lines_temp[j] == packages[i][3]:
+                    count += 1
+            if count == 3:
+                for k in range(4):
+                    lines_temp.append(packages[i][k])
+            lines_temp = sorted(list(set(lines_temp)))
+        if lines != lines_temp:
+            lines_temp = self.loop(lines_temp,packages)
+        return lines_temp   
+    
     def play(self, lines):
+        
+        packages = [[0] * 4]
+        for i in range(self.board**2):
+            packages.append([0] *4)
+            
+        k = 1
+        count = 0     
+        for i in range(self.board**2):
+            packages[i][0] = k
+            packages[i][1] = packages[i][0] + self.board
+            packages[i][2] = packages[i][1] + 1
+            packages[i][3] = packages[i][2] + self.board
+            count += 1 
+            if count == self.board:
+                count = 0
+                k += self.board + 2
+            else:
+                k += 1
+        lines = self.loop(lines,packages)
+        return lines
+    
+    def boardConstruct(self):
         Board = [[' '] * (2*self.board + 1)]
         for i in range(2 * self.board):
             Board.append([' '] * (2 * self.board + 1))
@@ -21,36 +59,12 @@ class Game():
                     if j%2 == 0:
                         Board[i][j] = k
                         k += 1
-                        
-        packages = [[0] * 4]
-        for i in range(self.board**2):
-            packages.append([0] *4)
-            
-        k = 1
-        count = 0     
-        for i in range(self.board**2):
-            packages[i][0] = k
-            packages[i][1] = packages[i][0] + self.board
-            packages[i][2] = packages[i][1] + 1
-            packages[i][3] = packages[i][2] + self.board
-            count += 1 
-            if count == self.board:
-                count = 0
-                k += self.board + 2
-            else:
-                k += 1   
+        return Board 
         
-        
-        for i in range(len(packages)):
-            count = 0
-            lines = sorted(lines) 
-            for j in range(len(lines)):
-                if lines[j] == packages[i][0] or lines[j] == packages[i][1] or lines[j] == packages[i][2] or lines[j] == packages[i][3]:
-                    count += 1
-            if count == 3:
-                for k in range(4):
-                    lines.append(packages[i][k])
-            lines = sorted(list(set(lines)))    
-        return lines    
-game = Game(4)
-game.play([1, 2, 3, 4, 6, 7, 9, 10, 11, 12, 13, 14, 16, 17, 18, 20, 21, 22, 23, 24])
+     
+game = Game(3)
+a = [5, 6, 12, 13, 16, 19, 20, 23] 
+b = [2, 5, 6, 9, 12, 13, 16, 19, 20, 23]
+print(game.play(a))
+print(b)
+

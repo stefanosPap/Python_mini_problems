9bdfc70eb20ae0d7cdda0deffc8860f2f07514ea
Stefanos <stefanospapadam@gmail.com>
2020-04-01 14:14:27 +0300

    Count squares Brute force solution

--- Count Squares In the Chess Board/count_chess.py
+++ Count Squares In the Chess Board/count_chess.py
@@ -12,19 +12,35 @@ def count(chessBoard):
     dictionary = {}    
     for i in range(2,len(chessBoard) + 1):
         dictionary.update({i: 0})
+    chessBoard = np.array(chessBoard)
+    for k in range(2,len(chessBoard) + 1):
+        for i in range(len(chessBoard) - k + 1):
+            for j in range(len(chessBoard) - k  + 1):
+                a = chessBoard[i:i+k,j:j+k]
+                if all(a.flatten()):
+                    dictionary[k] += 1
+    for k in range(2,len(chessBoard) + 1):
+        if dictionary[k] == 0:
+            del dictionary[k]
+    return dictionary
+'''        
     numpyChess = np.array(chessBoard)
-    k = 2
     l = len(numpyChess)
-    while True: 
-        cur = numpyChess[0:k,0:k].flatten()
-        if all(cur):
-            dictionary[k + 1] += 1        
-        k += 1 
-        if k == len(chessBoard):
-            break
-    l = len(numpyChess)
+    rows = slice(0,l)
+    columns = slice(0,l)
+    cur = numpyChess[rows,columns].flatten()
+    if all(cur):
+       dictionary[l] += 1        
+    
+    l = len(numpyChess)    
+    upperRight = numpyChess[::,l::-1]
+    downLeft = numpyChess[l::-1]
+    downRight = downLeft[::,l::-1]
+    
     while True:
-        cur = numpyChess[l::-1,0:l].flatten()
+        start = slice(l,None,-1)
+        end = slice(None,l+1)
+        cur = numpyChess[start,end].flatten()
         if all(cur):
             dictionary[l] += 1
         l -=1
@@ -47,8 +63,11 @@ def count(chessBoard):
         if l == 0:
             break
     return dictionary
+'''
 
-
+        
+        
+        
 chessBoard1=[
   [1,2,3,4,5],
   [6,7,8,9,10],
@@ -57,8 +76,8 @@ chessBoard1=[
   [21,22,23,24,25] 
 ]
 chessBoard=[
-  [1,1,1,1,1],
-  [1,1,1,1,1],
+  [1,0,1,1,1],
+  [1,1,1,0,1],
   [1,1,1,1,1],
   [1,1,1,1,1],
   [1,1,1,1,1] 

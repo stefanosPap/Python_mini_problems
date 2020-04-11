e6dea9e45df89497a95593377483def066a72431
Stefanos <stefanospapadam@gmail.com>
2020-04-11 23:40:09 +0300

    Try with approximately 77 right ans

--- LIne-Safari/line-safari.py
+++ LIne-Safari/line-safari.py
@@ -31,39 +31,64 @@ def line(grid):
         cor = []
         possibleRow = currentRow
         possibleCol = currentCol + 1
-        if possibleCol < len(grid[0]) and possibleCol >= 0:
+        if possibleCol < len(grid[0]) and possibleCol >= 0 and grid[currentRow][currentCol] != '|':
             cor.append([possibleRow,possibleCol])
+            if grid[currentRow][currentCol] == "+" and previousRow == possibleRow:
+                cor.pop()
+            
                 
         possibleCol = currentCol - 1
-        if possibleCol < len(grid[0]) and possibleCol >= 0:
+        if possibleCol < len(grid[0]) and possibleCol >= 0 and grid[currentRow][currentCol] != '|':
             cor.append([possibleRow,possibleCol])
-                 
+            if grid[currentRow][currentCol] == "+" and previousRow == possibleRow:
+                cor.pop()                 
+        
         possibleRow = currentRow + 1
         possibleCol = currentCol  
-        if possibleRow < len(grid) and possibleRow >= 0:
+        if possibleRow < len(grid) and possibleRow >= 0 and grid[currentRow][currentCol] != '-':
             cor.append([possibleRow,possibleCol])
-                
+            if grid[currentRow][currentCol] == "+" and previousCol == possibleCol:
+                cor.pop()                
                 
         possibleRow = currentRow - 1
-        if possibleRow < len(grid) and possibleRow >= 0:
+        if possibleRow < len(grid) and possibleRow >= 0 and grid[currentRow][currentCol] != '-':
             cor.append([possibleRow,possibleCol])
-        
+            if grid[currentRow][currentCol] == "+" and previousCol == possibleCol:
+                cor.pop()
+                
         count = 0
         for i in range(len(cor)):
-            if grid[cor[i][0]][cor[i][1]] != " " and [cor[i][0], cor[i][1]] != [previousRow, previousCol] and gridCopy[currentRow][currentCol] == 0:
+            if grid[cor[i][0]][cor[i][1]] != " " and [cor[i][0], cor[i][1]] != [previousRow, previousCol] and gridCopy[cor[i][0]][cor[i][1]] == 0:
                 nextMove = [cor[i][0], cor[i][1]]
                 count += 1
-        if count > 1 or count == 0:
-            return False 
-        if grid[currentRow][currentCol] == '+':
+        
+        if count == 0:
+            return False
+        
+        if grid[currentRow][currentCol] == "-":
+            if nextMove[1] == currentCol:
+                return False
+            if grid[nextMove[0]][nextMove[1]] == "|" :
+                return False
+        
+        if grid[currentRow][currentCol] == "|":
+            if nextMove[0] == currentRow:
+                return False
+            if grid[nextMove[0]][nextMove[1]] == "-" :
+                return False
+            
+        if grid[currentRow][currentCol] == "+":
+            if previousRow == nextMove[0]:
+                return False
+            if currentRow == nextMove[0] == "|":
+                return False
+            if currentCol == nextMove[1] == "-":
+                return False 
             if currentRow == previousRow == nextMove[0]:
                 return False
             elif currentCol == previousCol == nextMove[1]:
                 return False
-        if grid[nextMove[0]][nextMove[1]] == "|" and grid[currentRow][currentCol] == "-":
-            return False
-        elif grid[nextMove[0]][nextMove[1]] == "-" and grid[currentRow][currentCol] == "|":
-            return False 
+
         
         gridCopy[currentRow][currentCol] = 1
         previousRow = currentRow
@@ -75,7 +100,7 @@ def line(grid):
             break
     return True
 
-grid = ["                      ",
+grid1 = ["                      ",
         "   +-------+          ",
         "   |      +++---+     ",
         "X--+      +-+   X     "]
@@ -88,4 +113,6 @@ grid = ["    +----+",
         "    |+-+||",  
         "    +---+|",  
         "X--------+"]
-b = line(grid)
+grid = ["X+++ ",     
+        " +++X"]
+b = line(grid1)
